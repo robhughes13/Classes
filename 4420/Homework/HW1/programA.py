b89fc1f9a5e70f0fcc777d9065ccb86c2b9e8def
robhughes13 <81830990+robhughes13@users.noreply.github.com>
2022-09-16 00:54:39 -0400

    new pys

--- 4420/Homework/HW1/programA.py
+++ 4420/Homework/HW1/programA.py
@@ -1,1 +1,17 @@
-# Rob Hughes
+#Rob Hughes
+
+import random
+MAX_STATES=362880
+
+
+states= []
+while (len(states)< MAX_STATES):
+    count=0
+    temp=random.sample(range(9),9)
+    for i in range(len(states)):
+        if (temp==states[i]):
+            count+=1
+    if(count==0):
+        states.append(temp)
+for i in range(10):
+    print(states[i])
